c796b2a38a7463f9f03a58175ee466e9ab55c99d
Bobur Yusupov <bobur.yu@anur.tech>
2023-12-10 22:11:50 +0500

    Corrected

--- myapp/tasks/views.py
+++ myapp/tasks/views.py
@@ -8,13 +8,20 @@ from tasks.apps import TasksConfig
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+
+
 # Create your views here.
 def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
+        
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
     })
+
+"""
+You should add one line after each function
+"""
 def add(request):
     if request.method =="POST":
         form = NewTaskForm(request.POST)
